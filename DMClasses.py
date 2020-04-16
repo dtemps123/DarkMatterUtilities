62eb45bc4c31f7c4975d84af1ee74406b5622567
dylan <temples@u.northwestern.edu>
2020-04-16 12:57:11 -0500

    add Halo class and restructure velocity integrals

--- DMClasses.py
+++ DMClasses.py
@@ -1,5 +1,6 @@
 import numpy as n
 import matplotlib.pyplot as pyp
+from scipy.integrate import quad
 from scipy.special import erf, jv
 
 ## -- Unit Conversions -- ##
@@ -18,7 +19,7 @@ m_neutron_GeV	= 0.93957
 m_neutron_amu	= m_neutron_GeV * (1./amu_to_GeV)	#1.008664
 MW_esc_vel_ms	= 544e3
 Solar_vel_ms	= 230e3
-e_charge        = 1.602176634e-19
+e_charge		= 1.602176634e-19
 
 ## -- Class Definitions -- ##
 
@@ -120,10 +121,10 @@ class Target:
 		# [arXiv:0608035] Duda et al 2007 (consistent with DMCalc iumplementation)
 		_a 		= 0.52													# fm
 		_s 		= 0.9													# fm
-		_c      = (1.23 * n.power(self.A,1./3.)) - 0.60					# fm
-		_R1     = n.sqrt(  (_c**2) 
-			             + ( (7./3.)*(n.pi**2)*(_a**2) ) 
-			             - ( 5.0*(_s**2) )   )							# fm
+		_c	  = (1.23 * n.power(self.A,1./3.)) - 0.60					# fm
+		_R1	 = n.sqrt(  (_c**2) 
+						 + ( (7./3.)*(n.pi**2)*(_a**2) ) 
+						 - ( 5.0*(_s**2) )   )							# fm
 
 		_q 		= n.sqrt(2.0 * _Er_keV * self.NuclearMass_GeV)			# MeV / c
 		_qs		= (_s  / hbarc_MeV_fm) * _q								# dimensionless
@@ -179,6 +180,101 @@ class Target:
 		_beta 			= MW_esc_vel_ms / c_ms
 		return n.sqrt(1./2. * _threshold_GeV * _M_GeV / _beta**2)
 
+class Halo:
+	Model = 0
+	fVc_km_s = 220
+
+	def __init__(self, _HaloModel):
+		self.Model = _HaloModel
+
+	def GetHaloPDF_ms(self, _v_ms):
+		if (self.Model == 0):
+			return self.MaxwellBoltzmann_PDF_ms(_v_ms)
+		elif (self.Model == 1):
+			return self.StandardHaloModel_PDF_ms(_v_ms)
+		else:
+			return self.StandardHaloModel_PDF_ms(_v_ms)
+
+	def GetHaloIntegral_ms(self, _vmin_ms):
+		if (self.Model == 0):
+			return self.MaxwellBoltzmann_Integral_ms(_vmin_ms)
+		elif (self.Model == 1):
+			return self.StandardHaloModel_Integral_ms(_vmin_ms)
+		else:
+			return self.StandardHaloModel_Integral_ms(_vmin_ms)
+
+	def MaxwellBoltzmann_PDF_ms(self, _v_ms):
+		# Returns the value of the PDF (units of (m/s)^-1) given a velocity in m/s
+		# c.f. Anton N Baushev 2012 J. Phys.: Conf. Ser. 375 012048 [eq 1]
+		# Note this is independent of all dark matter particle properties
+		_V 			= MW_esc_vel_ms
+		_v0			= Solar_vel_ms
+		_norm 		= 1./ ( erf(_V/_v0) - (2./n.sqrt(n.pi))*(_V/_v0)* n.exp( - _V**2 / _v0**2 ) )
+		_exp_fac	= n.exp( - _v_ms**2 / _v0**2 )
+		_fac1		= 4. / n.sqrt(n.pi)
+		_fac2		= _v_ms**2 / _v0**3
+		MB_pdf      = _norm * _fac1 * _fac2 * _exp_fac
+		return MB_pdf / _v_ms
+
+	def MaxwellBoltzmann_Integral_ms(self, _vmin_ms):
+		if (_vmin_ms > MW_esc_vel_ms):
+			return 0
+		
+		_MBfactor   = quad(self.MaxwellBoltzmann_PDF_ms, _vmin_ms , MW_esc_vel_ms)
+		return _MBfactor[0]	# m^-1  x  s 
+
+	def StandardHaloModel_PDF_ms(self, _v_ms):
+		velocity  = _v_ms / 1e3  ## get this in km/s
+		v_esc_kms = MW_esc_vel_ms / 1e3
+		v_sol_kms = Solar_vel_ms  / 1e3
+
+		_k0 = n.power(n.pi * self.fVc_km_s*self.fVc_km_s, 1.5);
+		_k1 = ( k0 * erf(v_esc_kms/self.fVc_km_s)  
+		       - (2./sqrt(n.pi)) * sqrt(v_esc_kms/self.fVc_km_s) * n.exp(-n.power(v_esc_kms/self.fVc_km_s,2)) )
+		flab = velocity * (n.exp(-n.power((velocity - v_sol_kms)/self.fVc_km_s,2)) -
+		                   n.exp(-n.power((velocity + v_sol_kms)/self.fVc_km_s,2)))
+		return flab / (k1) * (n.pi *self.fVc_km_s*self.fVc_km_s/v_sol_kms)
+
+	def StandardHaloModel_Integral_ms(self, _vmin_ms):
+		## McCabe [arXiv:1005.0579]
+		x_esc	= (MW_esc_vel_ms*1e-3) / self.fVc_km_s
+		x_min	= (_vmin_ms*1e-3)	   / self.fVc_km_s
+		x_earth = (Solar_vel_ms*1e-3)  / self.fVc_km_s
+
+		zeta = 0 ## this will be in (km / s)^-1
+		beta = 1 ## this controls the escape velocity cut: beta=0 (hard cut), beta=1 (soft cut)
+
+		norm = (  n.power(n.pi,1.5)
+				* n.power(self.fVc_km_s,3.) 
+				* (  erf(x_esc) - 4./n.sqrt(n.pi)*n.exp(-x_esc*x_esc) 
+				   * (0.5*x_esc + beta*n.power(x_esc,3.)/3.)
+				  )
+			   )
+
+		if  ( x_earth+x_min < x_esc ):
+			zeta = ( n.power(n.pi,1.5)*n.power(self.fVc_km_s,2.)/(2.*norm*x_earth)
+					 * ( erf(x_min+x_earth) - erf(x_min-x_earth)
+					 - 4.*x_earth/n.sqrt(n.pi)*n.exp(-x_esc*x_esc)
+					 * (1 + beta*(x_esc*x_esc - x_earth*x_earth/3. - x_min*x_min)) )
+				   )
+
+		elif( x_min >= n.abs(x_esc-x_earth) and x_min < (x_earth+x_esc) ):
+			zeta = ( n.power(n.pi,1.5)*n.power(self.fVc_km_s,2.)/(2.*norm*x_earth)
+					 * ( erf(x_esc) + erf(x_earth-x_min)
+					 - 2./n.sqrt(n.pi)*n.exp(-x_esc*x_esc)
+					 *(x_esc + x_earth - x_min - beta/3.*(x_earth-2.*x_esc-x_min)
+					 *n.power(x_esc+x_earth-x_min,2.)) )
+				   )
+
+		elif( x_earth>x_min+x_esc ):
+			zeta = 1./(self.fVc_km_s*x_earth)
+
+		elif( (x_earth + x_esc) < x_min ):
+			zeta = 0  
+		 	
+		zeta_ms = zeta / 1e3
+		return zeta_ms
+
 # Defines a WIMP-like dark matter particle
 # Default is a 100 GeV/c^2 WIMP with local density of 0.3 GeV / cm^3
 class DarkMatter:
@@ -186,46 +282,9 @@ class DarkMatter:
 	Mass 	= 100.0				# GeV
 	Sigma 	= 1e-46				# cm^2
 	Rmass_DM_proton = 1.0		# kg
+	HaloModel = Halo(1)
 
 	def __init__(self, _Mass, _Sigma):
 		self.Mass 				= _Mass 		# Mass of DM particle in GeV/c^2
 		self.Sigma 				= _Sigma 		# Spin-independent DM-proton elastic scattering cross-section in cm^2
 		self.Rmass_DM_proton	= (( _Mass * m_proton_GeV ) / ( _Mass + m_proton_GeV)) * GeV_to_kg
-
-	def MaxwellBoltzmann_PDF_ms(self, _v):
-		# Returns the value of the PDF (units of (m/s)^-1) given a velocity in m/s
-		# c.f. Anton N Baushev 2012 J. Phys.: Conf. Ser. 375 012048 [eq 1]
-		# Note this is independent of all dark matter particle properties
-		_V 			= MW_esc_vel_ms
-		_v0			= Solar_vel_ms
-		_norm 		= 1./ ( erf(_V/_v0) - (2./n.sqrt(n.pi))*(_V/_v0)* n.exp( - _V**2 / _v0**2 ) )
-		_exp_fac	= n.exp( - _v**2 / _v0**2 )
-		_fac1		= 4. / n.sqrt(n.pi)
-		_fac2		= _v**2 / _v0**3
-		return _norm * _fac1 * _fac2 * _exp_fac
-
-	def Velocity_Dist_ms(self, _v):
-		# Turns the MB PDF into a distribution over which to integrate
-		dist = self.MaxwellBoltzmann_PDF_ms(_v) / _v
-		return dist
-
-	def Plot_VelocityDist(self):
-		_fig = pyp.figure()
-		_vel_array = n.linspace( 1e-3	, 600	, num=600)
-		_MB_dist = self.MaxwellBoltzmann_PDF_ms(_vel_array*1e3)
-		_DM_vel_dist = _MB_dist / _vel_array
-
-		_vel_array_esc = n.append( _vel_array[_vel_array <= MW_esc_vel_ms/1e3] , (MW_esc_vel_ms/1e3)+1. )
-		_DM_vel_dist_esc = n.append( _DM_vel_dist[_vel_array <= MW_esc_vel_ms/1e3] , 1e-10 )
-
-		pyp.semilogy(_vel_array, _DM_vel_dist, 'b:', label=r'MB $v_\mathrm{esc}=600$ km/s')
-		pyp.semilogy(_vel_array_esc, _DM_vel_dist_esc, 'b-', label=r'MB $v_\mathrm{esc}=544$ km/s')
-
-		pyp.xlim([0.,600.])
-		pyp.ylim([1e-10,1e-7])
-
-		pyp.xlabel("DM velocity [km/s]")
-		pyp.ylabel("Probability Density [a.u.]")
-
-		return _fig
-
