52345ef5060809539384060272b0f8afadd30d69
dylan <temples@u.northwestern.edu>
2020-06-15 10:47:32 -0500

    add files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dmutils',
+      version='1.0',
+      description='A plugin for calculating dark matter physics',
+      url='https://github.com/dtemps123/DarkMatterUtilities',
+      author='Dylan Temples',
+      author_email='dtemps123@gmail.com',
+      license='MIT',
+      packages=['dmutils'],
+      zip_safe=False)
