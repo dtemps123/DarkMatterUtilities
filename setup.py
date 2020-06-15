54e09b44d5be9fad180c7ecf96458df596ba8d1c
dylan <temples@u.northwestern.edu>
2020-06-15 10:59:44 -0500

    add extra fields

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='dmutils',
       version='1.0',
       description='A plugin for calculating dark matter physics',
+	  long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/dtemps123/DarkMatterUtilities',
       author='Dylan Temples',
       author_email='dtemps123@gmail.com',
       license='MIT',
-      packages=['dmutils'],
+      packages=setuptools.find_packages(),
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.6',
       zip_safe=False)
